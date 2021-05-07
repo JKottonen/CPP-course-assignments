05ef14e5dd47ab758d5353104d9b35a502411156
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-05-07 13:55:33 +0300

    Added assignments as comments

---
+++ vk5/teht.cpp
@@ -0,0 +1,9 @@
+// Toteuta funktio joka vaihtaa kahden int-tyyppisen luvun arvot,
+// käytä viittauksia
+
+// Toteuta funktio joka vaihtaa kahden int-tyyppisen luvun arvot,
+// käytä osoittimia
+
+// Toteuta funktiot joka tulostaa float-tyyppisen taulukon kaikki arvot,
+// hyödynnä osoitinaritmetiikkaa
+
