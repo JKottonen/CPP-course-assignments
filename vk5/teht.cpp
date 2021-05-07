382fc62eabf172679b98f6f31b2a1efe4f80ed27
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-05-07 14:11:58 +0300

    Added main method

--- vk5/teht.cpp
+++ vk5/teht.cpp
@@ -7,3 +7,19 @@
 // Toteuta funktiot joka tulostaa float-tyyppisen taulukon kaikki arvot,
 // hyödynnä osoitinaritmetiikkaa
 
+// HEADERS
+#include <iostream>
+
+using namespace std;
+
+main() {
+    int x = 2;
+    int y = 5;
+
+    int *ptrX = &x;
+    int *ptrY = &y;
+
+    cout << "pointer X: " << *ptrX << "pointer Y:" << *ptrY << endl;
+    cout << "X: " << *&x << "Y: " << *&y << endl;
+
+}
