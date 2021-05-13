e33304d77ce6349f383761b3ff905bf0bd44b5f5
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-05-13 20:42:03 +0300

    Started teht.cpp all over. There's only main() and a working printArray function to start with

--- vk5/teht.cpp
+++ vk5/teht.cpp
@@ -1,59 +1,22 @@
-// Toteuta funktio joka vaihtaa kahden int-tyyppisen luvun arvot,
-// käytä viittauksia
-
-// Toteuta funktio joka vaihtaa kahden int-tyyppisen luvun arvot,
-// käytä osoittimia
-
-// Toteuta funktiot joka tulostaa float-tyyppisen taulukon kaikki arvot,
-// hyödynnä osoitinaritmetiikkaa
-
 #include <iostream>
-
 using namespace std;
+ 
+// function declaration:
+void printArray(double arr[], int size);
 
-// HEADERS
-int changeInts(int x, int y);
-void printArray(float array[5]);
-void printPointer(int pointerX, int pointerY);
+int main () {
+   // an int array with 5 elements.
+   double balance[5] = {1000.5, 2.2, 3.5, 17.8, 50.2 };
 
-main() {
-    int x = 2;
-    int y = 5;
-    #define arrlength = 5
-    float array[5] = { 1.3, 2.5, 5.5, 1.5, 0.5 };
+   // pass pointer to the array as an argument.
+   printArray( balance, 5 );
     
-
-    int *ptrX = &x;
-    int *ptrY = &y;
-    float *ptrArray = &array[5];
-
-    cout << "pointer X: " << *ptrX << endl;
-    cout << "pointer Y: " << *ptrY << endl;
-    cout << "X: " << *&x << endl;
-    cout << "Y: " << *&y << endl;
-    cout << x << endl;
-    cout << y << endl;
-
-    printPointer(*ptrX, *ptrY);
-    printArray(&array);
-
+   return 0;
 }
 
-void printArray(float array[]) {
-    for (int i = 0; i < 5; i ++) {
-        cout << array[i] << endl;
-    }
-}
+void printArray(double arr[], int size) {      
 
-void printPointer( int pointerX , int pointerY) {
-    cout << pointerX << endl;
-    cout << pointerY << endl;
-
-    int temp = 0;
-    temp = pointerX;
-    pointerX = pointerY;
-    pointerY = temp;
-
-    cout << pointerX << endl;
-    cout << pointerY << endl;
+   for (int i = 0; i < size; ++i) {
+      cout << arr[i] << endl;
+   }
 }
