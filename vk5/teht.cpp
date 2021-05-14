92d24cecf67b68395d017ab6954f0aa4f0aa4014
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-05-14 13:38:48 +0300

    teht.cpp now has a double array printing fuction, supposedly working with pointer arithmetics

--- vk5/teht.cpp
+++ vk5/teht.cpp
@@ -10,23 +10,21 @@
 
 #include <iostream>
 using namespace std;
- 
+
 // function declaration:
 void printArray(double arr[], int size);
 
 int main () {
    // an int array with 5 elements.
    double balance[5] = {1000.5, 2.2, 3.5, 17.8, 50.2 };
+   double* p = balance;
 
    // pass pointer to the array as an argument.
-   printArray( balance, 5 );
+   printArray( p, 5 );
    return 0;
 }
 
-void printArray(double arr[], int size) {      
-    double *p;
-    p = arr;
-
+void printArray(double* p, int size) {
    for (int i = 0; i < size; ++i) {
       cout << *(p + i) << endl;
    }
