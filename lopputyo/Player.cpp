09e2b4279cc95e0a8bff634ad6c5cf8078ecd39d
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-07-26 20:16:17 +0300

    Removed unneccessary UI prints

--- lopputyo/Player.cpp
+++ lopputyo/Player.cpp
@@ -78,9 +78,11 @@ void Player::addShips(string shipSeed) {
 
 	int y, x;
 	char temp;
-    system("CLS");
-    Map.revealGrid();
+    // system("CLS");
+    // Map.revealGrid();
 	for(int i = 0; i < shipSeed.length(); i++) {
+        system("CLS");
+        Map.revealGrid();
 		int shipLength = (int)shipSeed[i]-48;
 		cout << "Pelaaja " << playerNumber << ": anna aloituskoordinaatit laivalle, jonka pituus on " << shipLength << endl;
 		cout << "A-J: ";
@@ -100,8 +102,7 @@ void Player::addShips(string shipSeed) {
 			cout << "EI KÄY" << endl;
 			i--;
 		}
-		system("CLS");
-        Map.revealGrid();
+
 	}
     system("pause");
 }
@@ -185,5 +186,5 @@ void Player::addShip(int y, int x, int length, char direction) {
             Map.grid[y][i] = 'O';
         }
     }
-    Map.revealGrid();
+//    Map.revealGrid();
 }
